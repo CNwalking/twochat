deee3fed234f89bef0d294b4cfe1f0c644b5b661
huqi <huqi@zhuojianchina.com>
2020-03-08 21:30:32 +0800

    start this project

---
+++ src/main/java/com/cnwalking/twochat/TwochatApplication.java
@@ -0,0 +1,13 @@
+package com.cnwalking.twochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwochatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwochatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cnwalking/twochat/TwochatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnwalking.twochat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwochatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
